8360539d1d345419012937801612df9a9dbc0e53
MPuzio <martynapuzio93@gmail.com>
2020-02-06 14:21:24 +0100

    edit function added

--- ajaxLista.js
+++ ajaxLista.js
@@ -1,68 +1,42 @@
 const HERO_URL =
   "https://us-central1-itfighters-hero.cloudfunctions.net/api/hero";
 window.onload = () => {
-  //pobieramy wszystkie elementy z listy w section na dole
-  const heroList = document.querySelector("#heroList");
-  const elSuperhero = document.querySelector("#superhero");
-  const elId = document.querySelector("#heroId");
-  const elHeroUrl = document.querySelector("#heroUrl");
-  const firstApp = document.querySelector("#FirstAppearance");
-  const characters = document.querySelector("#characters");
-  const publisher = document.querySelector("#publisher");
-  const description = document.querySelector("#description");
-  var addBtn = document.getElementById("addButton");
+  //pobieramy wszystkie elementy DOM
 
-  //pusta tablica z AllHeros gdzie bedziemy przchowywac odpowiedz z serwera
+  let heroList;
+  let elSuperhero;
+  let elId;
+  let elHeroUrl;
+  let firstApp;
+  let characters;
+  let publisher;
+  let description;
+  let addBtn;
+  let nameSuperhero;
+  let pictureInput;
+  let appearanceInput;
+  let charactersInput;
+  let publisherInput;
+  let descriptionInput;
+
   let allHeroes = [];
+  let edited = 0;
+  getFromServer();
+  generateList(allHeroes);
+  addButtonEvent();
 
-  //pobieramy z serwera liste i wrzucamy je do tablicy allHeros, nastepnie na podstawie
-  //tej tablicy generujemy liste elementow
   function getFromServer() {
     $.get(HERO_URL, function(responeFromServer) {
       allHeroes = responeFromServer;
       generateList(allHeroes);
     });
   }
-  // pobieramy dane z serwera - dziala
-  getFromServer();
 
-  //pobieramy value z inputow i na ich podstawie tworzymy nowego superbohatera - dziala!!!!
-  function createElement() {
-    debugger;
-    var nameSuperhero = document.getElementById("nameInput");
-    var picture = document.getElementById("pictureInput");
-    var appearance = document.getElementById("appearanceInput");
-    var characters = document.getElementById("caractersInput");
-    var publisher = document.getElementById("publisherInput");
-    var description = document.getElementById("descriptionInput");
-    let newHero = {
-      superhero: nameSuperhero.value,
-      publisher: publisher.value,
-      firstAppearance: appearance.value,
-      characters: characters.value,
-      url: picture.value,
-      description: description.value
-    };
-    $.post(HERO_URL, newHero).done(alert("Dodales element"));
-    clearList(heroList);
-    getFromServer();
-    generateList(allHeroes);
-  }
-
-  addBtn.addEventListener("click", function create(event) {
-    event.preventDefault();
-    createElement();
-  });
-  function clearList(list) {
-    list.innerHTML = [];
-  }
-  //tworzymy liste elementow z odpowiedzi z serwera, tworzymy buttony do obu, dodajemy im
-  //add event listenery i przypisujemy atrybuty id
   function generateList(heroes) {
     heroes.forEach(hero => {
+      bindDOMElements();
       const li = document.createElement("li");
       li.setAttribute("id", hero.id);
-
       li.innerText = hero.superhero + " : " + hero.publisher;
       const deleteBtn = document.createElement("button");
       deleteBtn.innerText = "Delete";
@@ -72,16 +46,14 @@ window.onload = () => {
       const editBtn = document.createElement("button");
       editBtn.innerText = "Edit";
       editBtn.setAttribute("id", hero.id);
-      // deleteBtn.addEventListener("click", editHero);
+      editBtn.addEventListener("click", editHero);
       li.appendChild(editBtn);
-
-      //poieramy dane z serwera dla kliknietego elementu i je wyswietlamy w hero list
-      //to wyswietlanie w hero list dziala
 
       li.addEventListener("click", event => {
         const liElement = event.target;
         const heroId = liElement.getAttribute("id");
         const heroDetailsURL = HERO_URL + "/" + heroId;
+
         $.get(heroDetailsURL, function(result) {
           console.log(result);
           elId.innerText = result.id;
@@ -95,29 +67,162 @@ window.onload = () => {
       });
       heroList.appendChild(li);
     });
+  }
+  function addButtonEvent() {
+    
+    let addBtn = document.getElementById("addButton");
 
-    function deleteHero(event) {
-      debugger;
+    addBtn.addEventListener("click", function create(event) {
       event.preventDefault();
+      createElement();
+    });
+  }
 
-      let idItemToRemove = parseInt(event.target.getAttribute("id")); // rzutuje stringa na numbera
-      const heroDetailsURL = HERO_URL + "/" + idItemToRemove;
-      console.log(idItemToRemove);
-      $.ajax({
-        type: "DELETE",
-        url: heroDetailsURL,
-        success: function(data) {
-          console.log(data);
-        },
-        error: function(data) {
-          console.log("Error:", data);
-        }
-      }).done(alert("Usunąłęś element"));
-      debugger;
-      console.log(heroList);
-      clearList(heroList);
-      getFromServer();
-      generateList(allHeroes);
+  function createElement() {
+    debugger;
+    bindDOMElements();
+
+    let newHero = {
+      superhero: nameSuperhero.value,
+      publisher: publisherInput.value,
+      firstAppearance: appearanceInput.value,
+      characters: charactersInput.value,
+      url: pictureInput.value,
+      description: descriptionInput.value
+    };
+    postNewHero(newHero);
+  }
+
+  function postNewHero(newHero) {
+    $.ajax({
+      type: "POST",
+      url: HERO_URL,
+      newHero,
+      success: function(data) {
+        console.log(data);
+        alert("Dodałeś element");
+        clearList(heroList);
+        getFromServer();
+        clearInputs();
+      },
+      error: function(data) {
+        console.log("Error:", data);
+      }
+    });
+  }
+
+  function editHero(element) {
+    debugger;
+    edited++;
+    let idItemToEdit = parseInt(element.target.getAttribute("id"));
+    const heroDetailsURL = HERO_URL + "/" + idItemToEdit;
+    // let heroToEditValue = allHeroes[idItemToEdit - 1];
+    // console.log(heroToEditValue);
+    getDetailsOfEditedItem(heroDetailsURL);
+
+    function getDetailsOfEditedItem(url) {
+      $.get(url, function(result) {
+        console.log(result);
+        createEdited(result);
+        let editedHero = createEdited(result);
+        addEditedOnClick(addBtn);
+        clearInputs();
+      });
     }
+    function createEdited(result) {
+      debugger;
+      bindDOMElements();
+      console.log(result.superhero);
+      nameSuperhero.value = result.superhero;
+      publisherInput.value = result.publisher;
+      pictureInput.value = result.url;
+      appearanceInput.value = result.firstAppearance;
+      charactersInput.value = result.characters;
+      descriptionInput.value = result.description;
+
+      let editedHero = {
+        superhero: nameSuperhero.value,
+        publisher: publisherInput.value,
+        firstAppearance: appearanceInput.value,
+        characters: charactersInput.value,
+        url: pictureInput.value,
+        description: descriptionInput.value
+      };
+      return editedHero;
+    }
+    if (edited !== 0) {
+      function addEditedOnClick(addBtn) {
+        addBtn.addEventListener("click", (editedHero, heroDetailsURL) => {
+          $.ajax({
+            type: "PUT",
+            url: heroDetailsURL,
+            editedHero,
+            success: function(data) {
+              console.log(data);
+              clearList(heroList);
+              getFromServer();
+              generateList(allHeroes);
+            },
+            error: function(data) {
+              console.log("Error:", data);
+            }
+          });
+        });
+      }
+    }
+  }
+
+  //pobieramy z serwera liste i wrzucamy je do tablicy allHeros, nastepnie na podstawie
+  //tej tablicy generujemy liste elementow
+
+  function clearList(list) {
+    list.innerHTML = [];
+  }
+
+  function clearInputs() {
+    let inputs = document.querySelectorAll(input);
+    inputs.forEach(input => {
+      input.innerText = "";
+    });
+  }
+
+  function deleteHero(event) {
+    debugger;
+    event.preventDefault();
+
+    let idItemToRemove = parseInt(event.target.getAttribute("id")); // rzutuje stringa na numbera
+    const heroDetailsURL = HERO_URL + "/" + idItemToRemove;
+    console.log(idItemToRemove);
+    $.ajax({
+      type: "DELETE",
+      url: heroDetailsURL,
+      success: function(data) {
+        console.log(data);
+        clearList(heroList);
+        getFromServer();
+      },
+      error: function(data) {
+        console.log("Error:", data);
+      }
+    }).done(alert("Usunąłęś element"));
+    debugger;
+    console.log(heroList);
+  }
+  function bindDOMElements() {
+    heroList = document.querySelector("#heroList");
+    elSuperhero = document.querySelector("#superhero");
+    elId = document.querySelector("#heroId");
+    elHeroUrl = document.querySelector("#heroUrl");
+    firstApp = document.querySelector("#FirstAppearance");
+    characters = document.querySelector("#characters");
+    publisher = document.querySelector("#publisher");
+    description = document.querySelector("#description");
+    addBtn = document.getElementById("addButton");
+    nameSuperhero = document.getElementById("nameInput");
+    pictureInput = document.getElementById("pictureInput");
+    appearanceInput = document.getElementById("appearanceInput");
+    charactersInput = document.getElementById("caractersInput");
+    publisherInput = document.getElementById("publisherInput");
+    descriptionInput = document.getElementById("descriptionInput");
   }
 };
